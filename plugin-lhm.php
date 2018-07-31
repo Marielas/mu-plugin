509bcaa7e04751b7e63bc67511340016cf324e55
Marielas <marielashh@gmail.com>
2018-07-31 18:05:39 +0200

    Update plugin-lhm.php

--- plugin-lhm.php
+++ plugin-lhm.php
@@ -5,3 +5,18 @@ Description: Plugin para ir poniendo mis funciones personalizadas
 Version: 0.1
 Author: Mario Hernanz
 */
+add_action( 'woocommerce_single_product_summary', 'hide_add_to_cart_button_variable_product', 1, 0 );
+function hide_add_to_cart_button_variable_product() {
+
+    // Removing add to cart button and quantities only
+    remove_action( 'woocommerce_single_variation', 'woocommerce_single_variation_add_to_cart_button', 20 );
+}
+function remove_add_to_cart(){
+    if ( has_term( 'oro', 'oro-1' ) ) {
+
+        remove_action( 'woocommerce_single_variation','woocommerce_single_variation_add_to_cart_button', 20 );
+}
+} 
+
+add_action('woocommerce_single_variation','remove_add_to_cart');
+
